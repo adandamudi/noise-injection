c550ee49b7a7ab02eb623ba4e4e1fd0d7c4d275f
Taj Shaik <tajshaik24@gmail.com>
2017-11-27 10:41:49 -0800

    push

--- background.js
+++ background.js
@@ -1,11 +1,24 @@
-/* 
+/*
  * Javascript file that can be running in the background, allowing
- * for searches to be generated when Chrome itself is not open. 
+ * for searches to be generated when Chrome itself is not open.
  */
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
 
 function click() {
 	alert('icon clicked');
 	chrome.tabs.executeScript({file: "script.js"}, function(response) {
+		chrome.tabs.create({"url": "https://www.amazon.com/Bose-SoundLink-Color-Bluetooth-speaker/dp/B01HETFQKS/ref=sr_1_2?s=electronics&ie=UTF8&qid=1511805879&sr=1-2&keywords=bose+speaker"}, function(tab){
+			sleep(2000).then(() => {chrome.tabs.remove(tab.id);})
+		});
+		chrome.tabs.create({"url": "https://www.amazon.com/Pampers-Swaddlers-Diapers-Newborn-Count/dp/B00B61HV8Q/ref=sr_1_6_a_it?ie=UTF8&qid=1511805942&sr=8-6&keywords=diapers"}, function(tab){
+			sleep(3000).then(() => {chrome.tabs.remove(tab.id);})
+		});
+		chrome.tabs.create({"url": "https://www.amazon.com/Assorted-Colorful-Kids-Pencils-Pack/dp/B000WEAJU0/ref=sr_1_12?s=office-products&ie=UTF8&qid=1511805963&sr=1-12&keywords=pencils"}, function(tab){
+			sleep(4000).then(() => {chrome.tabs.remove(tab.id);})
+		});
 		alert('finished');
 	});
 }
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-/* 
+/*
  * Javascript file that generates random searches for a plausible duration
- * and intervals once the extension has been clicked. 
+ * and intervals once the extension has been clicked.
  */
 
-/* 
- * Generates a random number of hours x between [1, 10] this process 
- * should run each time the chrome extension icon is clicked and generates 
+/*
+ * Generates a random number of hours x between [1, 10] this process
+ * should run each time the chrome extension icon is clicked and generates
  * searches once an hour for x hours.
  */
  function init() {
@@ -24,7 +24,7 @@
 
 }
 
-/* 
+/*
  * Returns a random integer between [1,upperBound].
  */
 function generateRandomNum(upperBound) {
@@ -36,11 +36,37 @@ function generateRandomNum(upperBound) {
 	return Math.ceil(randomFloat * upperBound);
 }
 
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
 
-/* 
- * Generates a series of random searches for the duration generated.
- */
 function generateSearches() {
 	var duration = generateRandomNum(30);
+  var data = {'profile1':[
+    {'url':'https://www.amazon.com/Bose-SoundLink-Color-Bluetooth-speaker/dp/B01HETFQKS/ref=sr_1_2?s=electronics&ie=UTF8&qid=1511805879&sr=1-2&keywords=bose+speaker'},
+      {"url":"https://www.amazon.com/Pampers-Swaddlers-Diapers-Newborn-Count/dp/B00B61HV8Q/ref=sr_1_6_a_it?ie=UTF8&qid=1511805942&sr=8-6&keywords=diapers"},
+      {"url":"https://www.amazon.com/Assorted-Colorful-Kids-Pencils-Pack/dp/B000WEAJU0/ref=sr_1_12?s=office-products&ie=UTF8&qid=1511805963&sr=1-12&keywords=pencils"}
+  ],
+   "profile2":[
+      {"url":"https://www.amazon.com/Sony-BDPS3700-Streaming-Blu-Ray-Player/dp/B01AT6B0DK/ref=sr_1_1?s=electronics&ie=UTF8&qid=1511805978&sr=1-1&keywords=blu+ray+player"},
+      {"url":"https://www.amazon.com/Stout-Hygiene-Bags-Dispenser-Stone/dp/B00UWK7YAW/ref=sr_1_3?s=electronics&ie=UTF8&qid=1511805992&sr=1-3&keywords=sanitary+napkins"},
+      {"url":"https://www.amazon.com/Purneat-Refrigerator-Filter-Frigidaire-ULTRAWF/dp/B075WV3R82/ref=sr_1_2_sspa?s=electronics&ie=UTF8&qid=1511806003&sr=1-2-spons&keywords=water&psc=1"}
+  ],
+   "profile3":[
+      {"url":"https://www.amazon.com/Premium-Capacitive-Stylus-iPhone-Digital/dp/B00P3KKCE6/ref=sr_1_26?s=electronics&ie=UTF8&qid=1511806018&sr=1-26&keywords=stylus"},
+      {"url":"https://www.amazon.com/Folgers-Classic-Ground-Coffee-Medium/dp/B010ULFOWC/ref=sr_1_4_s_it?s=grocery&ie=UTF8&qid=1511806068&sr=1-4&keywords=coffee"},
+      {"url":"https://www.amazon.com/Lindt-LINDOR-Chocolate-Truffles-Kosher/dp/B002RBTV78/ref=sr_1_5_a_it?ie=UTF8&qid=1511806037&sr=8-5&keywords=chocolate+gifts"}
+  ]};
 
+  chrome.tabs.create(data.profile1[0]);
+  chrome.tabs.create(data.profile1[1]);
+  chrome.tabs.create(data.profile1[2]);
 }
